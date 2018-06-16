c3444666b7711385a80628ee318373b968e0731b
Sefa Ümit Oray <sefa@fastmail.com>
2018-06-16 13:07:14 +0300

    :sunrise:

--- DailyCodingProblem10/Program.cs
+++ DailyCodingProblem10/Program.cs
@@ -1,12 +1,23 @@
 ﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace DailyCodingProblem10
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+        }
+
+        public static void CallWithDelay<T>(Func<T> func, int delay)
+        {
+            Task.Run(() =>
+            {
+                Thread.Sleep(TimeSpan.FromMilliseconds(delay));
+                func();
+            });
         }
     }
 }
---
+++ README.md
@@ -0,0 +1,5 @@
+# Daily coding problem 10 
+
+Implement a job scheduler which takes in a function f and an integer n, and calls f after n milliseconds.
+
+### Note: This is not official/best/preferred solution or anything like that. This is just how I solved it. If you are looking for challanges or solutions to Daily Coding Problems, please visit [DailyCodingProblem.com](https://www.dailycodingproblem.com/)
